9068ba0cb674bc60ac832babfa74cce4ea6e2565
Lewis Liu <lewisliuusa@outlook.com>
2021-05-21 11:43:03 -0700

    started working on bank. wired bankBtn to activity

--- app/src/main/java/dev/lewisliu/csi360midterm/MainActivity.java
+++ app/src/main/java/dev/lewisliu/csi360midterm/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     Button casinoBtn;
+    Button bankBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,16 +17,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         casinoBtn = (Button) findViewById(R.id.casinoBtn);
+        bankBtn = (Button) findViewById(R.id.bankBtn);
+
         casinoBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openCasino();
             }
         });
+
+        bankBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openBank();
+            }
+        });
     }
 
     public void openCasino() {
         Intent intent = new Intent(this, Casino.class);
+        startActivity(intent);
+    }
+
+    private void openBank() {
+        Intent intent = new Intent(this, Bank.class);
         startActivity(intent);
     }
 }
