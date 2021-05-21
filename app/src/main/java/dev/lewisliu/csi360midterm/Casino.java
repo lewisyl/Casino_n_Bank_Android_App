8979b6d68b3e29dc61250cca4903772f8ff5c8e5
Lewis Liu <lewisliuusa@outlook.com>
2021-05-20 17:40:23 -0700

    done casino page

---
+++ app/src/main/java/dev/lewisliu/csi360midterm/Casino.java
@@ -0,0 +1,17 @@
+package dev.lewisliu.csi360midterm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class Casino extends AppCompatActivity {
+    Button backHomeBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_casino);
+
+
+    }
+}
--- app/src/main/java/dev/lewisliu/csi360midterm/MainActivity.java
+++ app/src/main/java/dev/lewisliu/csi360midterm/MainActivity.java
@@ -2,13 +2,30 @@ package dev.lewisliu.csi360midterm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button casinoBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        casinoBtn = (Button) findViewById(R.id.casinoBtn);
+        casinoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCasino();
+            }
+        });
+    }
+
+    public void openCasino() {
+        Intent intent = new Intent(this, Casino.class);
+        startActivity(intent);
     }
 }
